5c550dc7793cfa89b8fe5e6db608e658be165f6f
Mina Mekhael <mina.mekhael@ymail.com>
2019-11-05 12:47:58 -0800

    heroku deploy fix

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20191025041348) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "recipes", force: :cascade do |t|
     t.string   "name",                                                                    null: false
     t.text     "ingredients",                                                             null: false
